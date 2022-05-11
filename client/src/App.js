c14c9f369dc016df474b043f8cbfa5077bfaaaf6
omer Korkmaz <h.omr.korkmaz@gmail.com>
2022-05-11 22:46:02 +0200

    added some styles to TODO form

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,5 @@
 import "./App.css";
-import TextField from "@mui/material/TextField";
-import Button from "@mui/material/Button";
+import { Grid, TextField, Button, Card, CardContent, Typography } from '@mui/material';
 import GET_TODOS from "./graphql/Queries";
 import { useQuery } from "@apollo/client";
 
@@ -14,11 +13,42 @@ function App() {
 
   return (
     <div className="App">
-      <form>
-        <TextField id="outlined-basic" label="Outlined" variant="outlined" />
-        <TextField id="outlined-basic" label="Outlined" variant="outlined" />
-        <Button variant="contained">Contained</Button>
-      </form>
+
+<div className="App"> 
+      <Typography gutterBottom variant="h3" align="center">
+        Graphql-React-Mongo-Express
+       </Typography>
+      <Grid>
+        <Card style={{ maxWidth: 450, padding: "20px 5px", margin: "0 auto" }}>
+          <CardContent>
+            <Typography gutterBottom variant="h5">
+              TODO LIST
+          </Typography> 
+            <Typography variant="body2" color="textSecondary" component="p" gutterBottom>
+              Fill up the TODO list and do it within 24 hours :).
+          </Typography> 
+            <form>
+              <Grid container spacing={1}>
+               
+                <Grid item xs={12}>
+                  <TextField type="email" placeholder="Title" label="Title" variant="outlined" fullWidth required />
+                </Grid>
+                
+                <Grid item xs={12}>
+                  <TextField label="Message" multiline rows={4} placeholder="Type your message here" variant="outlined" fullWidth required />
+                </Grid>
+                <Grid item xs={12}>
+                  <Button type="submit" variant="contained" color="primary" fullWidth>Submit</Button>
+                </Grid>
+
+              </Grid>
+            </form>
+          </CardContent>
+        </Card>
+      </Grid>
+    </div>
+
+
       <div>
         {data.getTodos.map((todo) => (
           <ul>
